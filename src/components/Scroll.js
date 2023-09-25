a5700231e0d50202b59a7c0b0760cbba288a6391
Giovanna Da Silva <giovannapdasilva@gmail.com>
2023-09-25 16:46:39 -0400

    Using Hooks rather than Class

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ import React from "react";
 const Scroll = (props) => {
   return (
     <div
-      style={{ overflowY: "scroll", borde: "1px solid black", height: "500px" }}
+      style={{ overflowY: "scroll", borde: "1px solid black", height: "100vh" }}
     >
       {props.children}
     </div>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,46 +1,59 @@
 import "./App.css";
-import React from "react";
+import React, {useState, useEffect} from "react";
 import CardList from "../components/CardList";
 import Scroll from "../components/Scroll";
 import SearchBox from "../components/SearchBox";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchField: "",
-    };
-  }
+function App(){
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     robots: [],
+  //     searchField: "",
+  //   };
+  // }
+  ////Using the useState Hook to replicate what was done with the constructor when App was a class
+  const [robots, setRobots] = useState([])
+ const [searchField, setSearchField] = useState('')
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
+  // componentDidMount() {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((response) => response.json())
+  //     .then((users) => this.setState({ robots: users }));
+  // }
+
+  //  const onSearchChange = (event) => {
+  //   this.setState({ searchField: event.target.value });
+  // };
+
+  useEffect(()=>{
+   fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }));
-  }
+      .then((users) => {setRobots(users)});
 
-  onSearchChange = (event) => {
-    this.setState({ searchField: event.target.value });
+  },[])
+
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value);
   };
 
-  render() {
-    const filteredRobots = this.state.robots.filter(robot => {
+    const filteredRobots = robots.filter(robot => {
       return robot.name
         .toLowerCase()
-        .includes(this.state.searchField.toLowerCase());
+        .includes(searchField.toLowerCase());
     });
 
-    return !this.state.robots.length ?
+    return !robots.length ?
     <h1>Loading...</h1>:(
       <div className="tc">
         <h1>Cat Friends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
+        <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <CardList robots={filteredRobots} />
         </Scroll>
       </div>
     );
-  }
+  
 }
 
 export default App;
